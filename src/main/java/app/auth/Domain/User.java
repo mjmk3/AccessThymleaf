0b072cc0c81c14285c1fff0b35009cca7dbcaf6f
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-03-10 20:49:20 +0300

    Thymeleaf

---
+++ src/main/java/app/auth/Domain/User.java
@@ -0,0 +1,30 @@
+package app.auth.Domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * @author MJ Makki
+ * @version 1.0
+ * @license SkyLimits, LLC (<a href="https://www.skylimits.tech">SkyLimits, LLC</a>)
+ * @email m.makki@skylimits.tech
+ * @since long time ago
+ */
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String phone;
+    private String password;
+}
