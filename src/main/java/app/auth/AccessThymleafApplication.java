c7c8de5aaa4773803dd142d6a36cccd1e6e95680
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-03-10 20:42:49 +0300

    Thymeleaf

---
+++ src/main/java/app/auth/AccessThymleafApplication.java
@@ -0,0 +1,13 @@
+package app.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessThymleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessThymleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/auth/AccessThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package app.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessThymleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
