1e4f8b8e43a09de410106db33a4e435020fff60d
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-03-11 00:16:29 +0300

    Thymeleaf

--- src/main/java/app/auth/Controller/UserController.java
+++ src/main/java/app/auth/Controller/UserController.java
@@ -1,7 +1,11 @@
 package app.auth.Controller;
 
+import app.auth.Domain.User;
+import app.auth.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author MJ Makki
@@ -14,13 +18,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/signUp")
-    public String SignUp(){
-        return "Sign_Up";
+    public String SignUp(Model model){
+        model.addAttribute("signUpRequest", new User());
+        return "sign_Up";
     }
 
     @GetMapping("/signIn")
-    public String SignIn(){
-        return "Sign_In";
+    public String SignIn(Model model){
+        model.addAttribute("signInRequest", new User());
+        return "sign_In";
+    }
+
+    @PostMapping("/signUp")
+    public String SignUp(@ModelAttribute User user){
+        System.out.println("SignUp Request" + user);
+        User signedUp = userService.SignUp(user.getPhone(), user.getPassword());
+        return signedUp == null ? "error" : "redirect/signIn";
     }
 }
